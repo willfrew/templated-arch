32464cb72c7923cdd90ee12746809fc194f501b7
Will Frew <will.frew1@gmail.com>
2021-03-06 09:05:52 +0200

    Fixes low battery notifications & pulls username out into a host/env var

--- roles/laptop/files/low-battery-suspend.sh
+++ roles/laptop/files/low-battery-suspend.sh
@@ -1,6 +1,7 @@
 #!/bin/env bash
 
 DEPS=(
+  'WF_USER'
   'WF_AC_CHARGING_FILE'
   'WF_BATTERY_NAME'
   'WF_AUTO_SUSPEND_BATTERY_PERCENTAGE'
@@ -13,6 +14,9 @@ for dep in "${DEPS[@]}"; do
   fi
 done
 
+# FIXME less hacky way of grabbing this
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u $WF_USER)/bus"
+
 if [[ $(cat /sys/class/power_supply/$WF_AC_CHARGING_FILE) -ne "1" ]]; then
   ## If battery is really low, just go ahead and suspend straight await.
   if [[ $(cat /sys/class/power_supply/$WF_BATTERY_NAME/capacity) -lt $WF_AUTO_SUSPEND_BATTERY_PERCENTAGE ]]; then
@@ -21,11 +25,11 @@ if [[ $(cat /sys/class/power_supply/$WF_AC_CHARGING_FILE) -ne "1" ]]; then
 
   ## Send a notification when battery percentage hits a low-ish level.
   if [[ $(cat /sys/class/power_supply/$WF_BATTERY_NAME/capacity) -eq $WF_LOW_BATTERY_NOTIFICATION_PERCENTAGE ]]; then
-    notify-send --urgency 'critical' "Low Battery $WF_LOW_BATTERY_NOTIFICATION_PERCENTAGE%"
+    su $WF_USER --command "notify-send --urgency 'critical' 'Low Battery $WF_LOW_BATTERY_NOTIFICATION_PERCENTAGE%'"
   fi
 else
   ## Send a notification when battery hits 99% as a friendly reminder to unplug the charger.
   if [[ $(cat /sys/class/power_supply/$WF_BATTERY_NAME/capacity) -gt 99 ]]; then
-    notify-send --urgency 'critical' --hint 'string:frcolor:#859900' "Battery Fully Charged" "Unplug from power"
+    su $WF_USER --command "notify-send --urgency 'critical' --hint 'string:frcolor:#859900' 'Battery Fully Charged' 'Unplug from power'"
   fi
 fi
